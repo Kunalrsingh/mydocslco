c67ed5dfdff394030ab83e420424f2222a7855ce
Kunal <ks231229@gmail.com>
2023-09-05 23:57:55 +0530

    added courses of array

--- index.js
+++ index.js
@@ -12,24 +12,37 @@ let courses = [
     {
         id: "11",
         name: "Learn Reactjs",
-        price: 299
+        price: 299,
     },
     {
         id: "22",
         name: "Learn Angularjs",
-        price: 299
+        price: 299,
     },
     {
         id: "33",
         name: "Learn Django",
-        price: 299
+        price: 299,
     },
-]
+];
 app.get("/",(req,res) =>{
     res.send("hello from lco");
 });
 app.get("/api/v1/lco",(req,res) =>{
     res.send("hello from lco docs");
+});
+
+app.get("/api/v1/lcoobject",(req,res) =>{
+    res.send({id: "55", name: "learn backend", price: 999});
+});
+
+app.get("/api/v1/courses",(req,res) =>{
+    res.send(courses);
+});
+
+app.get("/api/v1/mycourse/:courseId", (req,res) =>{
+    const myCourse = courses.find(course => course.id === req.params.courseId )
+    res.send(myCourse);
 });
 
 app.listen(4000,() => console.log('Server is  running at 4000 '));
