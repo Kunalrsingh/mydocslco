7da923ea19d042fa5034d99d97a959b50dfec962
Kunal <ks231229@gmail.com>
2023-09-07 23:45:32 +0530

    added url query route & docs

--- index.js
+++ index.js
@@ -52,4 +52,10 @@ app.post("/api/v1/addCourse", (req,res) => {
     res.send(true);
 });
 
+app.get("/api/v1/coursequery",(req,res) =>{
+let location = req.query.location
+let device = req.query.device
+res.send({location, device});
+});
+
 app.listen(4000,() => console.log('Server is  running at 4000 '));
